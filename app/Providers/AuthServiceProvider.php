91ca0a0cb5d408d7e6d686acf395a2a91f29a867
1357280829 <1357280829@qq.com>
2020-11-24 03:44:36 +0000

    delete answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,14 +2,17 @@
 
 namespace App\Providers;
 
-use App\Policies\QuestionPolicy;
+use App\Answer;
 use App\Question;
+use App\Policies\AnswerPolicy;
+use App\Policies\QuestionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     public function boot()
