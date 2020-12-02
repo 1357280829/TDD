56700694f868f4aece1e44916ea8531a7206eb66
1357280829 <1357280829@qq.com>
2020-12-02 08:57:51 +0000

    extract vote trait

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 
 class Answer extends Model
 {
+    use \App\Models\Traits\VoteTrait;
+
     protected $guarded = ['id'];
 
     public function question()
@@ -22,66 +24,5 @@ class Answer extends Model
     public function isBest()
     {
         return $this->id == $this->question->best_answer_id;
-    }
-
-    public function voteUp($user)
-    {
-        $attributes = ['user_id' => $user->id];
-
-        if (! $this->votes('vote_up')->where($attributes)->exists()) {
-            $this->votes('vote_up')->create(['user_id' => $user->id, 'type' => 'vote_up']);
-        }
-    }
-
-    public function votes($type)
-    {
-        return $this->morphMany(Vote::class, 'voted')->whereType($type);
-    }
-
-    public function cancelVoteUp($user)
-    {
-        $this->votes('vote_up')->where(['user_id' => $user->id, 'type' => 'vote_up'])->delete();
-    }
-
-    public function isVotedUp($user)
-    {
-        if (! $user) {
-            return false;
-        }
-
-        return $this->votes('vote_up')->where('user_id', $user->id)->exists();
-    }
-
-    public function getUpVotesCountAttribute()
-    {
-        return $this->votes('vote_up')->count();
-    }
-
-    public function voteDown($user)
-    {
-        $attributes = ['user_id' => $user->id];
-
-        if (! $this->votes('vote_down')->where($attributes)->exists()) {
-            $this->votes('vote_down')->create(['user_id' => $user->id, 'type' => 'vote_down']);
-        }
-    }
-
-    public function cancelVoteDown($user)
-    {
-        $this->votes('vote_down')->where(['user_id' => $user->id, 'type' => 'vote_down'])->delete();
-    }
-
-    public function isVotedDown($user)
-    {
-        if (! $user) {
-            return false;
-        }
-
-        return (bool)$this->votes('vote_down')->where('user_id', $user->id)->count();
-    }
-
-    public function getDownVotesCountAttribute()
-    {
-        return $this->votes('vote_down')->count();
     }
 }
